82cc37c7b45c01f344c6aec572314f8e0fc875f9
anfigeno <dolorcriticodevastador@proton.me>
2026-03-13 10:34:09 -0500

    Caracteristica registrosDeCanalesDeVoz refactorizada con contratos

--- src/caracteristicas/registrosDeDiscord/registrosDeCanalesDeVoz.ts
+++ src/caracteristicas/registrosDeDiscord/registrosDeCanalesDeVoz.ts
@@ -1,74 +1,85 @@
-import {
-	type Client,
-	ContainerBuilder,
-	Events,
-	TextDisplayBuilder,
-	type VoiceState,
-} from "discord.js";
+import { type Client, Events, type VoiceState } from "discord.js";
 import { canalDeRegistrosDeCanalesDeVoz } from "@/caches";
-import registro from "@/configuracion/registro";
-import { enviarRegistro } from "./util";
+import { Funci } from "@/lib/Funci";
+import { ErrorAlAsignarEventos, Registro, type SinEventosQueAsignar } from "./util";
 
 export default function establecerCaracteristicaDeRegistrosDeCanalesDeVoz(
 	cliente: Client,
 ): void {
-	cliente.on(Events.VoiceStateUpdate, registrarCambioDeEstado);
+	cliente.on(Events.VoiceStateUpdate, (ea, en) => new CambioDeEstado(ea, en).registrar());
 }
 
-function registrarCambioDeEstado(
-	estadoAnterior: VoiceState,
-	nuevoEstado: VoiceState,
-): void {
-	const usuario = nuevoEstado.member?.user || estadoAnterior.member?.user;
+abstract class RegistroDeCanalesDeVoz extends Registro {
+	protected override canalDeRegistros = canalDeRegistrosDeCanalesDeVoz;
+}
 
-	if (!usuario) {
-		registro.error("No se pudo obtener el usuario");
-		return;
+class CambioDeEstado extends RegistroDeCanalesDeVoz {
+	constructor(
+		private readonly estadoAnterior: VoiceState,
+		private readonly estadoNuevo: VoiceState,
+	) {
+		super();
 	}
 
-	let accion: string | null = null;
+	protected override asignarEventos(): Funci.Resultado<
+		null,
+		ErrorAlAsignarEventos | SinEventosQueAsignar
+	> {
+		const usuario = this.estadoNuevo.member?.user || this.estadoAnterior.member?.user;
 
-	if (!estadoAnterior.channelId && nuevoEstado.channelId) {
-		accion = `
-${usuario} se unió a ${nuevoEstado.channel}
-`;
-	} else if (estadoAnterior.channelId && !nuevoEstado.channelId) {
-		accion = `
-${usuario} se desconectó de ${estadoAnterior.channel}
-`;
-	} else if (estadoAnterior.channelId !== nuevoEstado.channelId) {
-		accion = `${usuario} se desconectó de ${estadoAnterior.channel} y se conectó a ${nuevoEstado.channel}`;
-	} else if (!estadoAnterior.selfMute && nuevoEstado.selfMute) {
-		accion = `${usuario} se muteó en ${nuevoEstado.channel}`;
-	} else if (estadoAnterior.selfMute && !nuevoEstado.selfMute) {
-		accion = `${usuario} se desmuteó en ${nuevoEstado.channel}`;
-	} else if (!estadoAnterior.selfDeaf && nuevoEstado.selfDeaf) {
-		accion = `${usuario} se silenció en ${nuevoEstado.channel}`;
-	} else if (estadoAnterior.selfDeaf && !nuevoEstado.selfDeaf) {
-		accion = `${usuario} se desilenció en ${nuevoEstado.channel}`;
-	} else if (!estadoAnterior.selfVideo && nuevoEstado.selfVideo) {
-		accion = `${usuario} activó su cámara en ${nuevoEstado.channel}`;
-	} else if (estadoAnterior.selfVideo && !nuevoEstado.selfVideo) {
-		accion = `${usuario} desactivó su cámara en ${nuevoEstado.channel}`;
-	} else if (!estadoAnterior.serverMute && nuevoEstado.serverMute) {
-		accion = `${usuario} fue muteado por un moderador`;
-	} else if (estadoAnterior.serverMute && !nuevoEstado.serverMute) {
-		accion = `${usuario} fue desmuteado por un moderador`;
-	} else if (!estadoAnterior.serverDeaf && nuevoEstado.serverDeaf) {
-		accion = `${usuario} fue silenciado por un moderador`;
-	} else if (estadoAnterior.serverDeaf && !nuevoEstado.serverDeaf) {
-		accion = `${usuario} fue desilenciado por un moderador`;
-	} else if (!estadoAnterior.streaming && nuevoEstado.streaming) {
-		accion = `${usuario} comenzó a transmitir`;
-	} else if (estadoAnterior.streaming && !nuevoEstado.streaming) {
-		accion = `${usuario} dejó de transmitir`;
+		if (!usuario) {
+			return Funci.fallo(
+				new ErrorAlAsignarEventos({ mensaje: "No se encontró el usuario" }),
+			);
+		}
+
+		if (!this.estadoAnterior.channelId && this.estadoNuevo.channelId)
+			this.eventos.push(`${usuario} se unió a ${this.estadoNuevo.channel}`);
+
+		if (this.estadoAnterior.channelId && !this.estadoNuevo.channelId)
+			this.eventos.push(`${usuario} se desconectó de ${this.estadoAnterior.channel}`);
+
+		if (this.estadoAnterior.channelId !== this.estadoNuevo.channelId)
+			this.eventos.push(
+				`${usuario} se desconectó de ${this.estadoAnterior.channel} y se conectó a ${this.estadoNuevo.channel}`,
+			);
+
+		if (!this.estadoAnterior.selfMute && this.estadoNuevo.selfMute)
+			this.eventos.push(`${usuario} se muteó en ${this.estadoNuevo.channel}`);
+
+		if (this.estadoAnterior.selfMute && !this.estadoNuevo.selfMute)
+			this.eventos.push(`${usuario} se desmuteó en ${this.estadoNuevo.channel}`);
+
+		if (!this.estadoAnterior.selfDeaf && this.estadoNuevo.selfDeaf)
+			this.eventos.push(`${usuario} se silenció en ${this.estadoNuevo.channel}`);
+
+		if (this.estadoAnterior.selfDeaf && !this.estadoNuevo.selfDeaf)
+			this.eventos.push(`${usuario} se desilenció en ${this.estadoNuevo.channel}`);
+
+		if (!this.estadoAnterior.selfVideo && this.estadoNuevo.selfVideo)
+			this.eventos.push(`${usuario} activó su cámara en ${this.estadoNuevo.channel}`);
+
+		if (this.estadoAnterior.selfVideo && !this.estadoNuevo.selfVideo)
+			this.eventos.push(`${usuario} desactivó su cámara en ${this.estadoNuevo.channel}`);
+
+		if (!this.estadoAnterior.serverMute && this.estadoNuevo.serverMute)
+			this.eventos.push(`${usuario} fue muteado por un moderador`);
+
+		if (this.estadoAnterior.serverMute && !this.estadoNuevo.serverMute)
+			this.eventos.push(`${usuario} fue desmuteado por un moderador`);
+
+		if (!this.estadoAnterior.serverDeaf && this.estadoNuevo.serverDeaf)
+			this.eventos.push(`${usuario} fue silenciado por un moderador`);
+
+		if (this.estadoAnterior.serverDeaf && !this.estadoNuevo.serverDeaf)
+			this.eventos.push(`${usuario} fue desilenciado por un moderador`);
+
+		if (!this.estadoAnterior.streaming && this.estadoNuevo.streaming)
+			this.eventos.push(`${usuario} comenzó a transmitir`);
+
+		if (this.estadoAnterior.streaming && !this.estadoNuevo.streaming)
+			this.eventos.push(`${usuario} dejó de transmitir`);
+
+		return Funci.exito(null);
 	}
-
-	if (!accion) return;
-
-	const resumen = new ContainerBuilder().addTextDisplayComponents(
-		new TextDisplayBuilder().setContent(accion),
-	);
-
-	enviarRegistro(resumen, canalDeRegistrosDeCanalesDeVoz);
 }
